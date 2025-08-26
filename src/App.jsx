63ed4736de240e21b11fbc6e28450340dd0ba7f3
External API <external-api@vibe-platform.com>
2025-08-26 17:51:34 +0000

    feat: add Hello World app in src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-900">Hello, world!</h1>
+    </div>
+  );
+}
